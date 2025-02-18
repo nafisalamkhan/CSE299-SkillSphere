04d8b761d3fd48e819f919d0864bd0b32b846ddf
Nafis Alam Khan <nafis.alam010@gmail.com>
2025-02-18 19:54:43 +0600

    file initialized

---
+++ src/test/java/com/cse299/skillSphere/SkillSphereApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cse299.skillSphere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillSphereApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
